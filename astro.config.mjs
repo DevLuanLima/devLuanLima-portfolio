41af3836b6c0d0b5eec48d9d514f728ab7a331cc
dev.lima <luan_marcell@outlook.com>
2023-07-11 17:37:12 -0300

    Style and Header

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,26 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import Icons from "unplugin-icons/vite";
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  vite: {
+    plugins: [
+      Icons({
+        compiler: "astro",
+      }),
+    ],
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: "@import 'src/styles/_mixins.scss';",
+        },
+      },
+    },
+  },
+  integrations: [
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+  ],
 });
